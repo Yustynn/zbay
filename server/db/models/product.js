48ed7c3d9523aa7a184547c6ad1c8486d2763e2c
Yustynn <yustynn@gmail.com>
2016-01-11 16:46:09 -0500

    Create Product model

---
+++ server/db/models/product.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  categories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  }],
+  description: {
+    type: String,
+    required: true
+  },
+  photoUrls: [String],
+  defaultPhotoUrl: String,
+  reviews: [{
+    type: mongoose.schema.Types.ObjectId,
+    ref: 'Review'
+  }],
+  stock: [{
+    type: Number,
+    default: 0
+  }]
+});
+
+// Sets default photo to first photo when no default photo is specified
+schema.pre('save', function (next) {
+    if (!this.defaultPhotoUrl && this.photoUrls) {
+      this.defaultPhotoUrl = this.photoUrls[0];
+    }
+    next();
+});
+
+mongoose.model('Product', schema);
