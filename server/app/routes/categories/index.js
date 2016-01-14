26cbb99dbe550daeae4229c4af999b97e65f0efb
Yustynn <yustynn@gmail.com>
2016-01-14 16:03:11 -0500

    Implement routesCrud helper in oredrs routes

--- server/app/routes/categories/index.js
+++ server/app/routes/categories/index.js
@@ -3,15 +3,8 @@ const router = require('express').Router();
 module.exports = router;
 
 import { mustBeAdmin } from '../../../helpers/routesPermissions'
-
 import { getDocAndDelete, getDocAndUpdate, getAllDocsAndSend, createDoc }
 from '../../../helpers/routesCrud';
-
-/**
- *  Admin  has access to all CRUD operations
- *  all users, including unregistered users can read categories
- *
- */
 
 router.get('/', getAllDocsAndSend('Category'));
 
