cede86c9f444d75d48ef5b32cb7c176f50a7c5ff
Jon Horn <jhorn85@gmail.com>
2016-01-15 17:34:20 -0500

    added state structure

---
+++ browser/js/common/directives/profile/address/address.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Jon on 1/15/16.
+ */
+app.directive('address', () => {
+    return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: '',
+        link: function(scope) {}
+    };
+});
