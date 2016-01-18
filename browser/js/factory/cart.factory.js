ea092ddfc3cdf8a348a2c0451122dbd2e2236971
Victor Hom <vh62@cornell.edu>
2016-01-17 20:22:43 -0500

    the cart factory deals with cart data and has localStorage in place

--- browser/js/factory/cart.factory.js
+++ browser/js/factory/cart.factory.js
@@ -1,13 +1,19 @@
 app.factory('CartFactory', () => {
   let factory = {};
+  if (!window.localStorage.getItem("cart")){
+    window.localStorage.setItem('cart', JSON.stringify([]));
+  }
 
-  factory.cart = [];
+  factory.cart = JSON.parse(localStorage.getItem("cart")) || [];
+
   factory.cartNumber = () => {
     return factory.cart.length;
   }
 
+  // when retrieving from local storage, JSON.parse(factory.cart)
   factory.addToCart = (product) => {
     factory.cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(factory.cart));
   }
 
   factory.removeFromCart = (product) => {
@@ -19,7 +25,11 @@ app.factory('CartFactory', () => {
     });
     if (cartIndex !== -1){
       factory.cart.splice(cartIndex, 1);
+      const storageCart = JSON.parse(localStorage.getItem("cart"));
+      storageCart.splice(cartIndex,1);
+      localStorage.setItem("cart", JSON.stringify(storageCart));
     }
+
 
   }
 
